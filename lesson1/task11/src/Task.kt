e1ca69caa5913ec26169f5a69ff745efd610c545
Aayush Kapoor <xeoneux@icloud.com>
2016-05-16 00:36:28 +0530

    Object expressions

--- lesson1/task11/src/Task.kt
+++ lesson1/task11/src/Task.kt
@@ -2,6 +2,8 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(x: Int, y: Int) = y - x
+    })
     return arrayList
 }
