d3afb2cebc88da064b9a31d831cf08ace4ceeb33
Aayush Kapoor <xeoneux@icloud.com>
2016-05-15 20:27:15 +0530

    Nullable types

--- lesson1/task8/src/Task.kt
+++ lesson1/task8/src/Task.kt
@@ -1,11 +1,16 @@
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
-){
-    TODO()
+) {
+    val personalInfo = client?.personalInfo
+    val email = personalInfo?.email
+
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
 }
 
-class Client (val personalInfo: PersonalInfo?)
-class PersonalInfo (val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
 interface Mailer {
     fun sendMessage(email: String, message: String)
 }
