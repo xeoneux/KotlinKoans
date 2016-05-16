98de1ae86e9159da2fce1c088bfd5944160452bc
Aayush Kapoor <xeoneux@icloud.com>
2016-05-16 12:40:11 +0530

    Comparison

--- lesson2/task1/src/Task.kt
+++ lesson2/task1/src/Task.kt
@@ -1,5 +1,9 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(other: MyDate): Int {
+        if (this.year != other.year) return (this.year - other.year)
+        if (this.month != other.month) return (this.month - other.month)
+        return (this.dayOfMonth - other.dayOfMonth)
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
