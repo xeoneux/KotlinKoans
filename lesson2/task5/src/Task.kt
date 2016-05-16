5a4032f7272927e15c3f14fbd3bea17883b1525b
Aayush Kapoor <xeoneux@icloud.com>
2016-05-16 16:36:35 +0530

    Operators overloading

--- lesson2/task5/src/Task.kt
+++ lesson2/task5/src/Task.kt
@@ -4,12 +4,18 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) = addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
