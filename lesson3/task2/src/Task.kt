461f90226093c09fef8d80990d9eca47ca75580b
Aayush Kapoor <xeoneux@icloud.com>
2016-05-16 16:43:12 +0530

    Filter map

--- lesson3/task2/src/Task.kt
+++ lesson3/task2/src/Task.kt
@@ -1,5 +1,5 @@
 // Return the set of cities the customers are from
-fun Shop.getCitiesCustomersAreFrom(): Set<City> = TODO()
+fun Shop.getCitiesCustomersAreFrom(): Set<City> = customers.map { it.city }.toSet()
 
 // Return a list of the customers who live in the given city
-fun Shop.getCustomersFrom(city: City): List<Customer> = TODO()
+fun Shop.getCustomersFrom(city: City): List<Customer> = customers.filter { it.city == city }
