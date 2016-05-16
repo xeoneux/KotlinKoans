dd0dfc64235e71ede50968aef680e3f1cb29c2de
Aayush Kapoor <xeoneux@icloud.com>
2016-05-16 12:45:46 +0530

    Range to

--- lesson2/task3/src/Task.kt
+++ lesson2/task3/src/Task.kt
@@ -1,6 +1,6 @@
-operator fun MyDate.rangeTo(other: MyDate) = TODO()
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in first..last
