6ec56a121cc0f41229318496d1b358f44696deb9
Aayush Kapoor <xeoneux@icloud.com>
2016-05-19 20:26:19 +0530

    Fold

--- lesson3/task10/src/Task.kt
+++ lesson3/task10/src/Task.kt
@@ -1,4 +1,8 @@
 // Return the set of products that were ordered by every customer
 fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
-    TODO()
+    val allProducts = customers.flatMap { it.orders.flatMap { it.products } }.toSet()
+
+    return customers.fold(allProducts, { orderedByAll, customer ->
+        orderedByAll.intersect(customer.orders.flatMap { it.products }.toSet())
+    })
 }
