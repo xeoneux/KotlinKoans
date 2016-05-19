f86c457c01abeca8f12ad83d56a0b0a609232a66
Aayush Kapoor <xeoneux@icloud.com>
2016-05-19 19:35:05 +0530

    FlatMap

--- lesson3/task4/src/Task.kt
+++ lesson3/task4/src/Task.kt
@@ -1,5 +1,5 @@
 // Return all products this customer has ordered
-fun Customer.getOrderedProducts(): Set<Product> = TODO()
+fun Customer.getOrderedProducts(): Set<Product> = orders.flatMap { it.products }.toSet()
 
 // Return all products that were ordered by at least one customer
-fun Shop.getAllOrderedProducts(): Set<Product> = TODO()
+fun Shop.getAllOrderedProducts(): Set<Product> = customers.flatMap { it.getOrderedProducts() }.toSet()
