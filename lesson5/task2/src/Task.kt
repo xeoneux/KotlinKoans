d12ca558e07f074ffee4eef03cf72780c7a8d2a5
Aayush Kapoor <xeoneux@icloud.com>
2016-05-21 23:36:02 +0530

    String and map builders

--- lesson5/task2/src/Task.kt
+++ lesson5/task2/src/Task.kt
@@ -1,6 +1,10 @@
 import java.util.HashMap
 
-/* TODO */
+fun <H, T> buildMap(build: MutableMap<H, T>.() -> Unit): Map<H, T> {
+    val map = HashMap<H, T>()
+    map.build()
+    return map
+}
 
 fun usage(): Map<Int, String> {
     return buildMap {
