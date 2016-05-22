a06a20de38dfd769fc8afe57742f769d80cba230
Aayush Kapoor <xeoneux@icloud.com>
2016-05-22 10:38:02 +0530

    Html builders

--- lesson5/task4/src/Task.kt
+++ lesson5/task4/src/Task.kt
@@ -1,7 +1,7 @@
 fun renderProductTable(): String {
     return html {
         table {
-            tr /* TODO */{
+            tr (color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -13,10 +13,22 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            TODO()
+            for ((index, product) in products.withIndex()) {
+                tr {
+                    td (color = getCellColor(index, 0)) {
+                        text(product.description)
+                    }
+                    td (color = getCellColor(index, 1)) {
+                        text(product.price)
+                    }
+                    td (color = getCellColor(index, 2)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(index: Int, row: Int) = if ((index + row) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(index: Int, row: Int) = if ((index + row) % 2 == 0) "#dce4ff" else "#eff2ff"
